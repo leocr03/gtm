6177a6386b4bf2a105ef376def77b8488cea4c81
leonardo <leocr03@gmail.com>
2017-02-07 22:10:13 -0200

    Starting creating front index page

--- back/server.js
+++ back/server.js
@@ -21,7 +21,14 @@ const PORT = 8000;
 // App
 const app = express();
 
-app.get('/insert', function (req, res) {
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE')
+  next();
+});
+
+app.post('/insert', function (req, res) {
     connect(function(db){
         console.log("inserting object in Mongo");
         insert(res, db);
@@ -35,10 +42,18 @@ app.get('/show', function (req, res) {
     });
 });
 
-app.get('/clean', function (req, res) {
+app.delete('/clean', function (req, res) {
     connect(function(db){
         console.log("cleaning objects in Mongo");
         clean(res, db);
+    });
+});
+
+
+app.get('/count', function (req, res) {
+    connect(function(db){
+        console.log("cleaning objects in Mongo");
+        count(res, db);
     });
 });
 
@@ -51,6 +66,7 @@ var insert = function(res, db) {
 
 var show = function(res, db) {
     db.collection("cats").find({}).toArray(function(err, data){
+        db.close();
         res.send(data);
     });
 };
@@ -60,7 +76,14 @@ var clean = function(res, db) {
         db.close();
         res.send("objects removed");
     });
-}
+};
+
+var count = function(res, db) {
+    db.collection("cats").count({}, function(err, result) {
+        db.close();
+        res.send(result);
+    });
+};
 
 app.listen(PORT);
 console.log('Running on http://localhost:' + PORT);
--- front/front.js
+++ front/front.js
@@ -4,7 +4,7 @@ function gtmController($scope, $http) {
     $scope.result = -1;
 
     $scope.insert = function() {
-        $http.post('/insert')
+        $http.post('http://localhost:8000/insert')
             .success(function(data) {
                 $scope.result = data;
                 console.log(data);
@@ -13,10 +13,10 @@ function gtmController($scope, $http) {
                 $scope.result = data;
                 console.log('Error: ' + data);
             });
-    }
+    };
 
     $scope.show = function() {
-        $http.get('/show')
+        $http.get('http://localhost:8000/show')
             .success(function(data) {
                 $scope.result = data;
                 console.log(data);
@@ -25,10 +25,10 @@ function gtmController($scope, $http) {
                 $scope.result = data;
                 console.log('Error: ' + data);
             });
-    }
+    };
 
     $scope.clean = function() {
-        $http.delete('/clean')
+        $http.delete('http://localhost:8000/clean')
             .success(function(data) {
                 $scope.result = data;
                 console.log(data);
@@ -37,5 +37,17 @@ function gtmController($scope, $http) {
                 $scope.result = data;
                 console.log('Error: ' + data);
             });
-    }
+    };
+
+    $scope.count = function() {
+        $http.get('http://localhost:8000/count')
+            .success(function(data) {
+                $scope.result = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                $scope.result = data;
+                console.log('Error: ' + data);
+            });
+    };
 }
